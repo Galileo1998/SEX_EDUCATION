89688c0f718e1cefadafe6f6f8b2589565dfe313
Galileo <galileoga63@gmail.com>
2019-07-10 01:27:24 -0600

    Cuarto commit
    Recta final

--- models/examendata.model.php
+++ models/examendata.model.php
@@ -27,4 +27,71 @@ function obtenerListas()
     return $modas;
 }
 
+function obtenerJuguetePorId($id)
+{
+  $sqlstr="select `juguetes`.`idjuguetes`,
+            `juguetes`.`nomjuguete`,
+            `juguetes`.`preciojuguete`,
+            `juguetes`.`estadojuguete`
+        from `examen`.`juguetes` where idjuguetes=%d";
+  $juguetes= array();
+  $juguetes=obtenerUnRegistro(sprintf($sqlstr, $id));
+  return $juguetes;
+}
+
+function obtenerEstados()
+{
+    return array(
+        array("cod"=>"ACT", "dsc"=>"Activo"),
+        array("cod"=>"INA", "dsc"=>"Inactivo"),
+        array("cod"=>"PLN", "dsc"=>"En Planificación"),
+        array("cod"=>"RET", "dsc"=>"Retirado"),
+        array("cod"=>"SUS", "dsc"=>"Suspendido"),
+        array("cod"=>"DES", "dsc"=>"Descontinuado")
+    );
+}
+
+function agregarNuevoJuguete($dscjuguete, $prcjuguete, $estjuguete) {
+    $insSql = "INSERT INTO juguetes(nomjuguete, preciojuguete, estadojuguete)
+      values ('%s', %f, '%s');";
+      if (ejecutarNonQuery(
+          sprintf(
+              $insSql,
+              $dscjuguete,
+              $prcjuguete,
+              $estjuguetes
+          )))
+      {
+        return getLastInserId();
+      } else {
+          return false;
+      }
+}
+
+function modificarJuguete($dscjuguete, $prcjuguete, $estjuguete, $idjuguete)
+{
+    $updSQL = "UPDATE juguetes set nomjuguete='%s', preciojuguete=%f,
+    estadojuguete='%s' where idjuguetes=%d;";
+
+    return ejecutarNonQuery(
+        sprintf(
+            $updSQL,
+            $dscjuguete,
+            $prcjuguete,
+            $estjuguete,
+            $idjuguete
+        )
+    );
+}
+function eliminarJuguete($idjuguete)
+{
+    $delSQL = "DELETE FROM juguetes where idjuguetes=%d;";
+
+    return ejecutarNonQuery(
+        sprintf(
+            $delSQL,
+            $idjuguete
+        )
+    );
+}
 ?>
